c5c3198d375ee7d056a180f23305d4b4fd9a6fd0
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-03-03 20:59:30 +0600

    added cors

--- lib_api/views.py
+++ lib_api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpRequest
-
+from django.http import HttpResponse,HttpRequest,JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie,csrf_exempt
+import json
 # Create your views here.
 
 
+@csrf_exempt
 def login(request: HttpRequest):
     if(request.method == 'POST'):
-        print(request.POST)
-    return HttpResponse("adsfasdf")
-    pass
+        x = json.loads(request.body)
+        print(x)
+        return HttpResponse("cookieafsdfasdf")
+    elif(request.method == 'GET'):
+        return HttpResponse("Aquired cookies")
