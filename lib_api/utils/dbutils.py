293eaa317db3798ea911c28b1b6752b85f84b668
Fahim Adnan <muhammadfahim010@gmail.com>
2024-04-20 20:33:21 +0600

    Update dbutils.py

--- lib_api/utils/dbutils.py
+++ lib_api/utils/dbutils.py
@@ -97,11 +97,10 @@ def get_category_books(q:str|None = None):
     try:
         args = [q]
         cursor.execute( '''
-        SELECT `info_id` ,`category`,`title`,`author`,`description`,`publication_year`,`edition`,COUNT(`book_id`) AS `available_books` 
+        SELECT `info_id` ,`category`,`title`,`author`,`description`,`publication_year`,`edition`
         FROM (book_category NATURAL JOIN book_info) NATURAL JOIN `book_copy`
         WHERE 
-            LOWER(book_category.category) = LOWER(%s) AND
-            `book_copy`.`is_available` = 1
+            LOWER(book_category.category) = LOWER(%s)
         GROUP BY `info_id` ,`category`,`title`,`author`,`description`,`publication_year`,`edition`
 
                         ''', args)
